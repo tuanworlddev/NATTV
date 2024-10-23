42c270507b3ba951ee58f82b6ed0494067704eca
tuanworlddev <nanhtuan.it@gmail.com>
2024-10-23 14:13:33 +0700

    init commit

---
+++ src/main/java/com/tuandev/nattv/NatTvApplication.java
@@ -0,0 +1,13 @@
+package com.tuandev.nattv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatTvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatTvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuandev/nattv/NatTvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandev.nattv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatTvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
